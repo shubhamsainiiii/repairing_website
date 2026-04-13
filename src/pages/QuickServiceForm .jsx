4bdf40e9139330309affcc21b8788ebb569a1f44
Shubham Saini <shubhamthoi27@gmail.com>
2026-04-13 23:31:07 +0530

    changes

--- src/pages/QuickServiceForm .jsx
+++ src/pages/QuickServiceForm .jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";  // Current page check karne ke liye
 import { motion } from "framer-motion";
